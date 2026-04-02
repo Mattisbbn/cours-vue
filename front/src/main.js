8d7309f543bcb1767c6e59299a7a93bc424e5ac9
mattisbbn <mattisbbn@gmail.com>
2026-04-02 14:08:04 +0200

    PWA ok

--- front/src/main.js
+++ front/src/main.js
@@ -1,9 +1,12 @@
 import './assets/main.css'
 import { createPinia } from 'pinia'
+import { registerSW } from 'virtual:pwa-register'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+registerSW({ immediate: true })
 
 const pinia = createPinia()
 const app = createApp(App)
--- front/vite.config.js
+++ front/vite.config.js
@@ -3,12 +3,46 @@ import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
 
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'], // Fichiers à mettre en cache
+      manifest: {
+        name: 'Nom complet de votre App',
+        short_name: 'NomCourt',
+        description: 'La description de votre application',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable', // TRÈS IMPORTANT pour Android
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
