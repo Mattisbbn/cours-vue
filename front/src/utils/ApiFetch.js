c54134cbc96d182efc177bc1fccf36b3c5f3b1d3
mattisbbn <mattisbbn@gmail.com>
2026-04-02 11:36:53 +0200

    Fix env

--- front/src/utils/ApiFetch.js
+++ front/src/utils/ApiFetch.js
@@ -2,7 +2,7 @@ const apiBaseUrl = import.meta.env.VITE_API_URL
 
 export const apiFetch = async (endpoint, options = {}) => {
   if (!apiBaseUrl) {
-    throw new Error('VITE_API_URL is not defined in front/.env')
+    throw new Error('VITE_API_URL is not defined in front/.env or front/.env.production')
   }
 
   return fetch(`${apiBaseUrl}${endpoint}`, {
